7c6624db25883e14ff1bef19acb8e26457d38702
Brian <brian@btforsythe.io>
2017-03-06 09:57:08 -0500

    initial commit

---
+++ src/test/java/org/wecancodeit/videostore/StubTest.java
@@ -0,0 +1,9 @@
+package org.wecancodeit.videostore;
+
+/**
+ * This is just a stub. It exists so that the project structure is added to git.
+ * (Git ignores empty directories.) Feel free to rename/delete it.
+ */
+public class StubTest {
+
+}
